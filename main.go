803f29582f2dff68059958a2a0b6ad226b696a19
nanih98 <49982093+nanih98@users.noreply.github.com>
2022-08-13 13:20:22 +0200

    Update go mod

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
-package main
+package gologger
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	"github.com/fatih/color"
+)
+
+var (
+	SuccessLogger *log.Logger
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
+)
+
+func init() {
+	// file, err := os.OpenFile("/tmp/logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	// if err != nil {
+	//     log.Fatal(err)
+	// }
+
+	InfoLogger = log.New(os.Stdout, color.Cyan("INFO: "), log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(os.Stdout, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(os.Stdout, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+}
 
 func main() {
-	fmt.Println("vim-go")
+	InfoLogger.Println("Starting the application...")
+	InfoLogger.Println("Something noteworthy happened")
+	WarningLogger.Println("There is something you should know about")
+	ErrorLogger.Println("Something went wrong")
 }
