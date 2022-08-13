cd94839c9b3d7d5177685dca697ee74eafd4c8ac
nanih98 <49982093+nanih98@users.noreply.github.com>
2022-08-13 17:14:27 +0200

    Fix: log.flags

--- main.go
+++ main.go
@@ -22,31 +22,34 @@ type CustomLogger struct {
 // Info prints output to the console colored and with and emoji
 func (l *CustomLogger) Info(msg string) {
 	l.Log.SetPrefix(fmt.Sprintf("%sINFO%s ", info, nocolor))
+	l.Log.SetFlags(log.Lshortfile)
 	l.Log.Printf("%s%s%s", info, msg, nocolor)
-
 }
 
 // Warn prints output to the console colored and with and emoji
 func (l *CustomLogger) Warn(msg string) {
 	l.Log.SetPrefix(fmt.Sprintf("%sWARNING%s ", warning, nocolor))
+	l.Log.SetFlags(log.Lshortfile)
 	l.Log.Printf("%s%s%s", warning, msg, nocolor)
 }
 
 // Fatal prints output to the console colored and with and emoji. Returns exit 1
 func (l *CustomLogger) Fatal(err error) {
 	l.Log.SetPrefix(fmt.Sprintf("%sFATAL%s ", errorf, nocolor))
+	l.Log.SetFlags(log.Lshortfile)
 	l.Log.Fatalf("%s%v%s", errorf, err, nocolor)
 }
 
 // Success prints output to the console colored and with and emoji
 func (l *CustomLogger) Success(msg string) {
 	l.Log.SetPrefix(fmt.Sprintf("%sSUCCESS%s ", success, nocolor))
+	l.Log.SetFlags(log.Lshortfile)
 	l.Log.Printf("%s%s%s", success, msg, nocolor)
 }
 
 // Logger is the main function
 func Logger() CustomLogger {
-	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime|log.Lshortfile)
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	return CustomLogger{Log: logger}
 }
